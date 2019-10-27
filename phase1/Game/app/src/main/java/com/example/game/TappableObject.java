7ef6ef7dc95572f0fbe88c1f4d3a8410dda55d6b
Nikola Marunic <nikola.marunic@mail.utoronto.ca>
2019-10-27 17:04:27 -0400

    Created class TappableObject for use in levels 1 and 3.

---
+++ phase1/Game/app/src/main/java/com/example/game/TappableObject.java
@@ -0,0 +1,10 @@
+package com.example.game;
+
+import android.view.MotionEvent;
+
+public class TappableObject extends GameObject {
+    public boolean isTapped(MotionEvent event) {
+        //TODO
+        return false;
+    }
+}

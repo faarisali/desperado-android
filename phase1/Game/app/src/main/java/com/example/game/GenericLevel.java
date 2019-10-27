ad836b94a3f08696ea677bac2f66719dcd1adacb
Mohammad Faaris Ali <faaris.ali@mail.utoronto.ca>
2019-10-27 15:20:23 -0400

    Created getters and setters for instance variables in Generic Level.

--- phase1/Game/app/src/main/java/com/example/game/GenericLevel.java
+++ phase1/Game/app/src/main/java/com/example/game/GenericLevel.java
@@ -6,15 +6,30 @@ import android.media.Image;
 import java.util.ArrayList;
 
 public abstract class GenericLevel {
+
     private Image backgroundImage;
     private ArrayList<GameObjects> gameObjects;
 
     public GenericLevel(Image backgroundImage) {
         this.backgroundImage = backgroundImage;
     }
-    public abstract void draw(Canvas canvas);
 
+    public abstract void draw(Canvas canvas);
     public abstract void update();
 
+    public Image getBackgroundImage() {
+        return backgroundImage;
+    }
 
+    public void setBackgroundImage(Image backgroundImage) {
+        this.backgroundImage = backgroundImage;
+    }
+
+    public ArrayList<GameObjects> getGameObjects() {
+        return gameObjects;
+    }
+
+    public void setGameObjects(ArrayList<GameObjects> gameObjects) {
+        this.gameObjects = gameObjects;
+    }
 }

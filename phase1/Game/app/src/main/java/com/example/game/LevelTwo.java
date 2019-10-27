0e954ea9da225c1fc4cf717caf0254fdb69e826f
Syed Saqib Ali <syeds.ali@mail.utoronto.ca>
2019-10-27 16:02:37 -0400

    Clarified documentation and update contructor for LevelTwo.

--- phase1/Game/app/src/main/java/com/example/game/LevelTwo.java
+++ phase1/Game/app/src/main/java/com/example/game/LevelTwo.java
@@ -1,18 +1,26 @@
 package com.example.game;
 
 import android.graphics.Canvas;
+import android.media.Image;
 
 public class LevelTwo extends GenericLevel {
 
     private float movementSpeed;
 
-    public LevelTwo() {
+    /**
+     * Constructs a GenericLevel
+     *
+     * @param backgroundImage background image for this level.
+     * @param lives           the number of lives the player starts with on this level.
+     */
+    public LevelTwo(Image backgroundImage, int lives) {
+        super(backgroundImage, lives);
     }
 
     /**
      * Need to draw background, character and obstacles.
      *
-     * @param canvas
+     * @param canvas where the img is drawn
      */
     @Override
     public void draw(Canvas canvas) {
@@ -20,7 +28,9 @@ public class LevelTwo extends GenericLevel {
     }
 
 
-    //Create a new obstacle
+    /**
+     * Spawn a new obstacle (may depend on player has ran so far)
+     */
     public void spawnObstacle() {
 
     }

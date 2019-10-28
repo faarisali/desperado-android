847a2b40d25a062570123cd0ad6b7dd1e86412cf
Kevin Ge <kevin.ge@mail.utoronto.ca>
2019-10-28 13:47:32 -0400

    "Implemented obstacle spawner (spawns in random intervals)"

--- phase1/Game/app/src/main/java/com/example/game/LevelTwo.java
+++ phase1/Game/app/src/main/java/com/example/game/LevelTwo.java
@@ -4,11 +4,37 @@ import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
 import android.view.MotionEvent;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class LevelTwo extends GenericLevel {
 
     private float movementSpeed;
     private PlayerLevelTwo player = new PlayerLevelTwo();
-    private Obstacle cactus = new Obstacle(500, 500, 3);
+    private ArrayList<Obstacle> obstacleList = new ArrayList<Obstacle>();
+    private int groundY = 500;
+    private float defaultObstacleMoveSpeed = 9;
+
+    //TimerTask subclass, deals with random spawning of obstacles
+    private class SpawnObstacleTask extends TimerTask {
+        private LevelTwo l2;
+        private Timer timer = new Timer();
+
+        public SpawnObstacleTask(LevelTwo l2) {
+            this.l2 = l2;
+        }
+
+        @Override
+        public void run() {
+            int delay = (1 + new Random().nextInt(2)) * 1000;
+            timer.schedule(new SpawnObstacleTask(this.l2), delay);
+            Obstacle obstacle = new Obstacle(1000, groundY, defaultObstacleMoveSpeed);
+            this.l2.obstacleList.add(obstacle);
+        }
+    }
+
     /**
      * Constructs a GenericLevel
      *
@@ -16,10 +42,11 @@ public class LevelTwo extends GenericLevel {
      * @param lives           the number of lives the player starts with on this level.
      */
     public LevelTwo(Drawable backgroundImage, int lives) {
-
+        new SpawnObstacleTask(this).run();
     }
 
     public LevelTwo() {
+        new SpawnObstacleTask(this).run();
     }
 
     /**
@@ -30,15 +57,21 @@ public class LevelTwo extends GenericLevel {
     @Override
     public void draw(Canvas canvas) {
         player.draw(canvas);
-        spawnObstacle(canvas);
+        drawObstacles(canvas);
     }
 
 
     /**
-     * Spawn a new obstacle (may depend on player has ran so far)
+     * draws every obstacle in managed in this Level2
+     *
+     * @param canvas
      */
-    public void spawnObstacle(Canvas canvas) {
-        cactus.draw(canvas);
+    private void drawObstacles(Canvas canvas) {
+        for (Obstacle o :
+                obstacleList) {
+            o.draw(canvas);
+        }
+
     }
 
     @Override
@@ -47,12 +80,21 @@ public class LevelTwo extends GenericLevel {
     }
 
     /**
+     * updates every obstacle in managed in this Level2
+     */
+    private void updateObstacles() {
+        for (Obstacle o :
+                obstacleList) {
+            o.move();
+        }
+    }
+    /**
      * update movement of player (jumps), spawning of obstacles and update
      */
     @Override
     public void update() {
         player.move();
-        cactus.move();
+        updateObstacles();
     }
 
 

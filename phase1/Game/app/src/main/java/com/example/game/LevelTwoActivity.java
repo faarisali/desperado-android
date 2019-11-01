be0c5556a06a080e2011813a2265847c1616cf56
Syed Saqib Ali <syeds.ali@mail.utoronto.ca>
2019-11-01 18:32:07 -0400

    Bug fixes after last merge.

---
+++ phase1/Game/app/src/main/java/com/example/game/LevelTwoActivity.java
@@ -0,0 +1,20 @@
+package com.example.game;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class LevelTwoActivity extends AbstractActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        currView = new GameView(this);
+        GameManager game = currView.gameManager;
+        game.changeLevel(2);
+        setContentView(currView);
+    }
+
+}

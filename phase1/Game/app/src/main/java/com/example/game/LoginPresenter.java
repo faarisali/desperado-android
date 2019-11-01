21ea77811dac411dd0d237b78ae11807fb4ebf04
Mohammad Faaris Ali <faaris.ali@mail.utoronto.ca>
2019-10-31 22:23:51 -0400

    Implemented way to sign up with a local account.

---
+++ phase1/Game/app/src/main/java/com/example/game/LoginPresenter.java
@@ -0,0 +1,37 @@
+package com.example.game;
+
+import android.content.Context;
+
+public class LoginPresenter {
+    private LoginView loginView;
+    private  LoginModel loginModel;
+
+    LoginPresenter(LoginView loginView) {
+        this.loginView = loginView;
+        this.loginModel = new LoginModel(this);
+    }
+
+    public void login(String username, String password) {
+        loginModel.login(username, password);
+    }
+
+    public void signup(String username, String password) {
+        loginModel.signup(username, password);
+    }
+
+    public void notifyError() {
+        if (loginView != null) {
+            loginView.displayError();
+        }
+    }
+
+    public void notifySuccess() {
+        if (loginView != null) {
+            loginView.navigateToHome();
+        }
+    }
+
+    Context getContext() {
+        return loginView.getContext();
+    }
+}

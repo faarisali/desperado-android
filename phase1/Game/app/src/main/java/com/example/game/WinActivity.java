0e9c1b46bb45dde87f63867078d5a1d9ae7a84c2
Nikola Marunic <nikola.marunic@mail.utoronto.ca>
2019-11-01 14:01:50 -0400

    Made it so the back button doesn't return to previous activity. Also disabled the restart button until the LevelOneActivity is up and working.

--- phase1/Game/app/src/main/java/com/example/game/WinActivity.java
+++ phase1/Game/app/src/main/java/com/example/game/WinActivity.java
@@ -53,14 +53,16 @@ public class WinActivity extends AppCompatActivity implements View.OnClickListen
             case R.id.mainMenuButton:
                 Intent intent = new Intent(this, MainMenu.class);
                 startActivity(intent);
+                finish();
                 break;
-            case R.id.restartButton:
-                Intent intent2 = new Intent(this, LevelThreeActivity.class);
-                intent2.putExtra("Points", 0);
-                intent2.putExtra("Gold", 0); //Temporary until all are converted to activities
-                intent2.putExtra("Lives", 3);
-                startActivity(intent2);
-                break;
+//            case R.id.restartButton: //For future when all activities are working
+//                Intent intent2 = new Intent(this, LevelThreeActivity.class);
+//                intent2.putExtra("Points", 0);
+//                intent2.putExtra("Gold", 0); //Temporary until all are converted to activities
+//                intent2.putExtra("Lives", 3);
+//                startActivity(intent2);
+//                finish();
+//                break;
         }
     }
 }

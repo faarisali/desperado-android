123eeb1d90d197e3c95af2d7b25ba6d1f9c693a0
Kevin Ge <kevin.ge@mail.utoronto.ca>
2019-11-01 00:17:24 -0400

    "added lives to level 2, added countdown for level 2"

--- phase1/Game/app/src/main/java/com/example/game/LevelTwo.java
+++ phase1/Game/app/src/main/java/com/example/game/LevelTwo.java
@@ -1,5 +1,6 @@
 package com.example.game;
 
+import android.content.Intent;
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
 import android.view.MotionEvent;
@@ -15,7 +16,8 @@ public class LevelTwo extends GenericLevel {
     private int groundY = 500;
     private float defaultObstacleMoveSpeed = 9;
     private PlayerLevelTwo player = new PlayerLevelTwo(groundY);
-
+    private int lives;
+    private Timer time = new Timer();
 
     /**
      * Constructs a GenericLevel
@@ -24,11 +26,22 @@ public class LevelTwo extends GenericLevel {
      */
     public LevelTwo(int lives) {
         new SpawnObstacleTask(this).run();
+        this.lives = lives;
     }
 
     public LevelTwo() {
         new SpawnObstacleTask(this).run();
         isRunning = true;
+        this.lives = 3;
+        this.time.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                returnToMain();
+            }
+        }, 5000);//5 second countdown
+    }
+
+    private void returnToMain() {
     }
 
     /**
@@ -82,11 +95,18 @@ public class LevelTwo extends GenericLevel {
         }
     }
 
+    private void updateLives() {
+        this.lives--;
+        if (this.lives == 0) {
+            returnToMain();
+        }
+    }
+
     private void detectCollisions() {
         for(Obstacle item: obstacleList) {
             if (-40 < player.x - item.x && player.x - item.x < 45){
                 if (player.y - item.y > - 60)
-                    isRunning = false;
+                    updateLives();
             }
         }
     }

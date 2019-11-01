a6c884aa8f6ceb9180861be9db9547610c298c23
Alexander Xu Chen <alexxu.chen@mail.utoronto.ca>
2019-11-01 00:04:56 -0400

    Added customization for player costume in MainMenu

--- phase1/Game/app/src/main/java/com/example/game/MainMenu.java
+++ phase1/Game/app/src/main/java/com/example/game/MainMenu.java
@@ -1,15 +1,53 @@
 package com.example.game;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.ImageViewCompat;
+
+import java.util.List;
 
 public class MainMenu extends AppCompatActivity {
+
+    /** The character display*/
+    private ImageView character;
+
+    /** The customization button*/
+    private Button customizeButton;
+
+    /** The current costume of the character*/
+    private int currentCostume;
+
+    /** A list of costumes*/
+    private int[] costumes = {
+            R.drawable.cowboy_blue,
+            R.drawable.cowboy_sheriff,
+            R.drawable.cowboy_bandit,
+            R.drawable.cowboy_yellow
+    };
+
     public static final String USERNAME = "username";
+
+    public void customizeCharacter() {
+        character = findViewById(R.id.costume);
+        customizeButton = findViewById(R.id.costumeButton);
+        customizeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentCostume++;
+                currentCostume = currentCostume % costumes.length;
+                character.setImageResource(costumes[currentCostume]);
+            }
+        });
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +56,7 @@ public class MainMenu extends AppCompatActivity {
         String message = intent.getStringExtra(USERNAME);
         TextView usernameText = findViewById(R.id.usernameTextView);
         usernameText.setText(message);
+        customizeCharacter();
     }
 
     /**

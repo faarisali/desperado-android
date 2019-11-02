ed3c736afceeda642e28bdbee784ad385c702947
Syed Saqib Ali <syeds.ali@mail.utoronto.ca>
2019-11-01 23:00:10 -0400

    Did not push a file last commit.

---
+++ phase1/Game/app/src/main/java/com/example/game/GameManagerObserver.java
@@ -0,0 +1,24 @@
+package com.example.game;
+
+public class GameManagerObserver {
+    AbstractActivity activity;
+
+    public GameManagerObserver(AbstractActivity a) {
+        activity = a;
+
+    }
+
+    public void update(GameManager game) {
+        if (!game.isCurrLevelRunning() && !game.isPaused) {
+            if (game.getCurrLives() == 0) {
+                activity.loseGame(game);
+            } else {
+                System.out.println(game.getCurrLives());
+                activity.winGame(game);
+            }
+        }
+//        else if (game.isPaused) {
+//            activity.pause();
+//        }
+    }
+}

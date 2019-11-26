dc3a82788e32529dcda44fa4775cb3d065b05f87
Mohammad Faaris Ali <faaris.ali@mail.utoronto.ca>
2019-11-26 12:08:20 -0500

    Added improved documentation to the login system.

--- phase1/Game/app/src/main/java/com/example/game/Login/LoginMapDatabase.java
+++ phase1/Game/app/src/main/java/com/example/game/Login/LoginMapDatabase.java
@@ -1,13 +1,16 @@
 package com.example.game.Login;
 
+/**
+ * Interface for a Map based database for the login system.
+ */
 interface LoginMapDatabase {
-    public  void save (String username, String password);
-    public  String load (String key);
-    public  User getCurrentUser();
-    public  void setCurrentUser(User user);
+    public  void save (String key, String value); // Save a string key pair value
+    public  String load (String key); // Load a value from the database given a key
+    public  User getCurrentUser(); // return  the currently logged in user
+    public  void setCurrentUser(User user); // store the current user
 
-    public void addUser(User user);
-    public User getUser(String username);
+    public void addUser(User user); // add a user account to the database
+    public User getUser(String username); //return a user given a username from the database
 
-    public User addDefaultUser(String username, String password);
+    public User addDefaultUser(String username, String password); // add a default user to the database
 }

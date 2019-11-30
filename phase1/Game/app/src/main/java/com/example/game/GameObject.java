ed1989aa71489a5ce0451c1e6a7557a131e0e439
Nikola Marunic <nikola.marunic@mail.utoronto.ca>
2019-11-30 15:37:27 -0500

    Created javadoc for the smaller level one specific classes and GameObject.

--- phase1/Game/app/src/main/java/com/example/game/GameObject.java
+++ phase1/Game/app/src/main/java/com/example/game/GameObject.java
@@ -1,7 +1,14 @@
 package com.example.game;
 
 public abstract class GameObject {
+    /**
+     * the X coordinate of this GameObject.
+     */
     public int x;
+
+    /**
+     * the Y coordinate of this GameObject.
+     */
     public int y;
 
     public GameObject(int newX, int newY) {
@@ -9,18 +16,38 @@ public abstract class GameObject {
         y = newY;
     }
 
+    /**
+     * gets the x value for this object.
+     *
+     * @return this object's X coordinate.
+     */
     public int getX() {
         return x;
     }
 
+    /**
+     * gets the y value for this object.
+     *
+     * @return this object's Y coordinate.
+     */
     public int getY() {
         return y;
     }
 
+    /**
+     * sets the value of this objects X coordinate.
+     *
+     * @param x the new X coordinate.
+     */
     public void setX(int x) {
         this.x = x;
     }
 
+    /**
+     * sets the value of this objects Y coordinate.
+     *
+     * @param y the new Y coordinate.
+     */
     public void setY(int y) {
         this.y = y;
     }

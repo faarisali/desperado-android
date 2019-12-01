90fca2550bf1f0b20d7a8f8957651497f36091ac
Kevin Ge <kevin.ge@mail.utoronto.ca>
2019-12-01 01:27:43 -0500

    Added DamageScreen.java java docs.

--- phase1/Game/app/src/main/java/com/example/game/LevelTwo/DamageScreen.java
+++ phase1/Game/app/src/main/java/com/example/game/LevelTwo/DamageScreen.java
@@ -7,12 +7,25 @@ import java.util.ArrayList;
 public class DamageScreen extends GameObject {
 
     int x, y;
+
+    /**
+     * Integer acting as a boolean for whether or not the damage screen should be drawn
+     */
     int shouldDisplay = 0;
 
+    /**
+     * Instantiates a DamageScreen game object.
+     *
+     * @param x x coordinate of top left corner
+     * @param y y coordinate of top left corner
+     */
     public DamageScreen(int x, int y) {
         super(x, y);
     }
 
+    /**
+     * Draws for the damage screen for 4 cycles of drawing
+     */
     public void buffer() {
         if (shouldDisplay >= 1) {
             shouldDisplay++;
@@ -22,6 +35,10 @@ public class DamageScreen extends GameObject {
         }
     }
 
+    /**
+     *
+     * @return an array list contain x, y and shouldDisplay;
+     */
     public ArrayList<Integer> draw() {
         ArrayList<Integer> temp = new ArrayList<>();
         temp.add(x);
